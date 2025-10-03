978e54936a58339e2f79cdd050b232418675b204
sei-shissam <shissam@sei.cmu.edu>
2025-10-03 17:46:17 -0500

    bump version number

--- scir-oss.sh
+++ scir-oss.sh
@@ -30,7 +30,7 @@
 # bash exitpoint search down for _cleanup_and_exit (often rearchable from _fatal)
 #
 
-readonly _version="pubRel 251002 (branch: publicRelease)"
+readonly _version="pubRel 251003 (branch: publicRelease)"
 
 #
 # check_runtime will confirm these settings
